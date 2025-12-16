f19f4a1fcc99cd4410db7fbb4307df915d8297f0
Seth De Preter <seth.de.preter@student.ehb.be>
2025-12-16 16:24:29 +0100

    Finetuning models

---
+++ CafeTerminal.Shared/Models/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CafeTerminal.Shared.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int TableId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public bool IsClosed { get; set; }
+        public List<OrderLine> OrderLines { get; set; } = new List<OrderLine>();
+    }
+}
---
+++ CafeTerminal.Shared/Models/OrderLine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CafeTerminal.Shared.Models
+{
+    public class OrderLine
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; } = string.Empty;
+        public decimal UnitPrice { get; set; }
+        public int Quantity { get; set; }
+    }
+}
