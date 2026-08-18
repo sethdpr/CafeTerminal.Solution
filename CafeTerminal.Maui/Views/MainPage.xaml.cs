f083332abd8827054e0db09bb03644f1e2766114
Seth De Preter <seth.de.preter@student.ehb.be>
2026-08-18 13:56:07 +0200

    remove test api code

--- CafeTerminal.Maui/Views/MainPage.xaml.cs
+++ CafeTerminal.Maui/Views/MainPage.xaml.cs
@@ -15,45 +15,6 @@ public partial class MainPage : ContentPage
         _authService = new AuthService();
     }
 
-    private async void OnTestApiClicked(object sender, EventArgs e)
-    {
-        try
-        {
-            var response = await _apiService.GetAsync("/api/Main/test");
-
-            if (response.IsSuccessStatusCode)
-            {
-                var result = await response.Content.ReadAsStringAsync();
-
-                await DisplayAlert(
-                    "API test",
-                    result,
-                    "OK");
-            }
-            else if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
-            {
-                await DisplayAlert(
-                    "Niet ingelogd",
-                    "De API heeft de JWT geweigerd.",
-                    "OK");
-            }
-            else
-            {
-                await DisplayAlert(
-                    "API fout",
-                    $"Statuscode: {(int)response.StatusCode}",
-                    "OK");
-            }
-        }
-        catch (Exception ex)
-        {
-            await DisplayAlert(
-                "Fout",
-                $"Er kon geen verbinding worden gemaakt met de API: {ex.Message}",
-                "OK");
-        }
-    }
-
     private async void OnLogoutClicked(object sender, EventArgs e)
     {
         await _authService.LogoutAsync();
